5d01110533306cb2dff8bf586b05c60a52175902
Danila Slesarev <blackdeer1524@yandex.ru>
2025-08-17 16:11:04 +0300

    major fixes

--- planner.py
+++ planner.py
@@ -1,6 +1,7 @@
 import datetime
 from fractions import Fraction
 from dataclasses import dataclass
+import math
 
 
 @dataclass(frozen=True)
@@ -9,38 +10,35 @@ class SubjectInfo:
     total_count: int
 
 
-subjects_info: dict[str, SubjectInfo] = {
-    "Algebra": SubjectInfo(
-        already_done=14,
-        total_count=33
-    ),
-    "Probability": SubjectInfo(
-        already_done=0,
-        total_count=27,
-    )
-}
-
 TODAY = datetime.datetime.today()
 DEADLINE = datetime.datetime(TODAY.year, 8, 31)
-DAYS_TOTAL = (DEADLINE - TODAY).days + 1
+DAYS_LEFT = (DEADLINE - TODAY).days + 1
+
+subjects_info: dict[str, SubjectInfo] = {
+    "Algebra": SubjectInfo(already_done=21, total_count=35),
+    "GraphDBMS": SubjectInfo(already_done=0, total_count=DAYS_LEFT),
+}
+
 
 subjects_count = len(subjects_info)
-total_lectures_count = sum(info.total_count - info.already_done for info in subjects_info.values())
+total_lectures_count = sum(
+    info.total_count - info.already_done for info in subjects_info.values()
+)
 
-dayly_load = Fraction(total_lectures_count, DAYS_TOTAL)
+dayly_load = Fraction(total_lectures_count, DAYS_LEFT)
 current_dose = Fraction(0, 1)
 
 # сколько уже сделано
-subjects_goals = {
-    name: Fraction(info.total_count - info.already_done, info.total_count)
+done_percentages = {
+    name: Fraction(0, info.total_count - info.already_done)
     for name, info in subjects_info.items()
 }
 
 # считаем с конца. иначе план может на сегодня ничего не выдать
-today = DEADLINE
-next_lecture_to_schedule = {s: info.total_count for s, info in subjects_info.items()}
-for i in range(DAYS_TOTAL):
-    cur_date = today - datetime.timedelta(days=i)
+today = TODAY
+next_lecture_to_schedule = {s: info.already_done + 1 for s, info in subjects_info.items()}
+for i in range(DAYS_LEFT):
+    cur_date = today + datetime.timedelta(days=i)
     current_dose += dayly_load
     actual_count = int(current_dose)
     current_dose -= actual_count
@@ -49,15 +47,19 @@ for i in range(DAYS_TOTAL):
         continue
 
     for j in range(actual_count):
-        least_done_percent = Fraction(-1, 1)
+        least_done_percent = Fraction(2, 1)
         least_done_subject: str | None = None
-        for subject, percentage_done in subjects_goals.items():
-            if least_done_percent < percentage_done:
+        for subject, percentage_done in done_percentages.items():
+            if percentage_done < least_done_percent:
                 least_done_percent = percentage_done
                 least_done_subject = subject
         assert least_done_subject is not None
-        print(f"[{cur_date.date()}]:{least_done_subject} {next_lecture_to_schedule[least_done_subject]}")
-        subjects_goals[least_done_subject] -= Fraction(
-            1, subjects_info[least_done_subject].total_count
+        print(
+            f"[{cur_date.date()}]:{least_done_subject} {next_lecture_to_schedule[least_done_subject]}"
         )
-        next_lecture_to_schedule[least_done_subject] -= 1
+        done_percentages[least_done_subject] += Fraction(
+            1,
+            subjects_info[least_done_subject].total_count
+            - subjects_info[least_done_subject].already_done,
+        )
+        next_lecture_to_schedule[least_done_subject] += 1
